0f825665fea241f48b9b85ca44cd1ef0dcc7a837
honghanh426417 <114908283+honghanh426417@users.noreply.github.com>
2024-01-20 10:09:03 +0700

    add

---
+++ HRManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HRManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
